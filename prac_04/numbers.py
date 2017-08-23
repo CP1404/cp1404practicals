4fc96f78610392c4e474778245b89d9a3e23aa3c
David Plummer <david.plummer1@my.jcu.edu.au>
2017-08-23 19:38:54 +1000

    Numbers prac_04

--- prac_04/numbers.py
+++ prac_04/numbers.py
@@ -1,3 +1,12 @@
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
 print(numbers[0])
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3" in numbers)
+print(numbers + [6, 5, 3])
