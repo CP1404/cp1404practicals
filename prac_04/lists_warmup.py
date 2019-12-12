3932f16f6057b304165d65ca37ecedc3bb3cbfff
CaoHan13779469 <58327414+CaoHan13779469@users.noreply.github.com>
2019-12-12 16:32:29 +0800

    Create lists_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,20 @@
+numbers=[3, 1, 4, 1, 5, 9, 2]
+#numbers[0] = 3
+#numbers[-1] = 2
+#numbers[3] = 1
+#numbers[:-1] = 3, 1, 4, 1, 5, 9
+#numbers[3:4] = 1
+#5 in numbers = True
+#7 in numbers = False
+#"3" in  numbers = False
+#numbers + [6, 5, 3] = 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+
+numbers[0] = "ten"
+print(numbers)
+
+numbers[-1] = 1
+print(numbers)
+
+print(numbers[2:])
+
+print(9 in numbers)
